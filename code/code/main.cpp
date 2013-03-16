d552383696e18dce042d4af1698163290572e419
Lim Byunghoon <seian@me.com>
2013-03-16 22:23:12 +0900

    getRandomNumber(), duplicationProtect(), numSort() added.

--- code/code/main.cpp
+++ code/code/main.cpp
@@ -1,10 +1,21 @@
+/*
+ * The lottery managing application.
+ * Private purpose only.
+ * no commercial.
+ */
+
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
 using namespace std;
 
+#define MAX_ARR 6
+
 void showMenu(void);
 void lotteryPractice(void);
+int getRandomNumber(void);
+bool duplicationProtect(int *numAdr, int place, int cmpNum);
+void numSort(int *numAdr);
 
 int main()
 {
@@ -41,18 +52,59 @@ void showMenu(void)
 
 void lotteryPractice(void)
 {
-    int numbers[6];
-    int lotteryNumbers[6];
+    int numbers[MAX_ARR];
+    int lotteryNumbers[MAX_ARR];
     
     cout << "=========================" << endl;
     cout << "LOTTERY PRACTICE"          << endl;
     cout << "=========================" << endl;
     cout << "INSERT YOUR SIX NUMBERS"   << endl;
-    cout << "=> "                       << endl;
-    for(int i=0;i<6;i++)
+    cout << "=> "                              ;
+    for(int i=0;i<MAX_ARR;i++)
     {
         lotteryNumbers[i] = rand()%50 + 1;
         cin >> numbers[i];
     }
-    
+    numSort(numbers);
+
 }
+
+int getRandomNumber(void)
+{
+    return 0;
+}
+
+bool duplicationProtect(int *numAdr, int place, int cmpNum)
+{
+    for(int i=0;i<place;i++)
+        if(*(numAdr + i) == cmpNum)
+            return false;
+    return true;
+}
+
+
+void numSort(int *numAdr)
+{
+    for(int i=0;i<MAX_ARR-1;i++)
+        for(int j=i+1;j<MAX_ARR;j++)
+            if(*(numAdr + i) > *(numAdr + j))
+            {
+                int tmp;
+                tmp = *(numAdr + j);
+                *(numAdr + j) = *(numAdr + i);
+                *(numAdr + i) = tmp;
+            }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
