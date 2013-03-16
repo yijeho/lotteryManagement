94d68c39d17ebe0f880ea301e956120420e34436
Lim Byunghoon <seian@me.com>
2013-03-16 21:01:00 +0900

    test

---
+++ code/code/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  code
+//
+//  Created by Lim Byunghoon on 13. 3. 16..
+//  Copyright (c) 2013년 Lim Byunghoon. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
